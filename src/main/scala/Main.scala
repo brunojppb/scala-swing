6d99a4e3c5da9db0534a82af764a5d8af779e0f6
Bruno Paulino <brunojppb@gmail.com>
2017-02-19 15:18:20 +0100

    first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+import ui.UI
+
+/**
+  * Created by bruno on 19/02/17.
+  */
+object Main extends App {
+  override def main(args: Array[String]): Unit = {
+
+    val ui = new UI()
+    ui.visible = true
+
+  }
+}
---
+++ src/main/scala/ui/UI.scala
@@ -0,0 +1,13 @@
+package ui
+import scala.swing._
+
+/**
+  * Created by bruno on 19/02/17.
+  */
+class UI extends MainFrame {
+
+  title = "GUI Program 1"
+  preferredSize = new Dimension(320, 240)
+  contents = new Label("First label on screen!")
+
+}
