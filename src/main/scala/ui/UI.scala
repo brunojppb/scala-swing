fcdd2a2c6d478b1addf080ffa8307aeb2e8f3c3e
Bruno Paulino <brunojppb@gmail.com>
2017-02-19 16:16:08 +0100

    Textfields, TextArea, Toggle, Checkbox, Combobox and RadioButtons

--- src/main/scala/ui/UI.scala
+++ src/main/scala/ui/UI.scala
@@ -6,39 +6,82 @@ import scala.swing._
   */
 class UI extends MainFrame {
 
-  title = "GUI Program 1"
+  title = "GUI Program"
 
-  val label = new Label("My first UI")
+  def restrictHeight(s: Component): Unit = {
+    s.maximumSize = new Dimension(Short.MaxValue, s.preferredSize.height)
+  }
+
+  val nameField = new TextField(columns = 32)
+  val likeScala = new CheckBox("I like Scala")
+  likeScala.selected = true
+  val status1 = new RadioButton("Radio 1")
+  val status2 = new RadioButton("Radio 2")
+  val status3 = new RadioButton("Radio 3")
+  status3.selected = true
+  val statusGroup = new ButtonGroup(status1, status2, status3)
+  val gender = new ComboBox(List("Other", "Male", "Female"))
+  val commentField = new TextArea {
+    rows = 8
+    lineWrap = true
+    wordWrap = true
+  }
+  val pressMe = new ToggleButton("Press me")
+  pressMe.selected = true
+
+  restrictHeight(nameField)
+  restrictHeight(gender)
 
   contents = new BoxPanel(Orientation.Vertical) {
-    contents += label
-    contents += Swing.VStrut(10)
-    contents += Swing.Glue
-    contents += Button("Press me!") { pressMeAction() }
-    contents += Swing.VStrut(5)
-    contents += Button("Change Text") { changeTextAction() }
-    contents += Swing.VStrut(5)
-    contents += Button("Close") { closeMe() }
-    border = Swing.EmptyBorder(10, 10, 10, 10)
-  }
-
-  def pressMeAction(): Unit = {
-    Dialog.showMessage(contents.head, "Thanks!", title = "You pressed me!")
-  }
-
-  def changeTextAction(): Unit = {
-    val r = Dialog.showInput(contents.head, "Enter New Text:", initial = label.text)
-    r match {
-      case Some(text) => label.text = text
-      case None =>
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += new Label("My Name")
+      contents += Swing.HStrut(5)
+      contents += nameField
     }
+
+    contents += Swing.VStrut(5)
+    contents += likeScala
+    contents += Swing.VStrut(5)
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += status1
+      contents += Swing.HStrut(10)
+      contents += status2
+      contents += Swing.HStrut(10)
+      contents += status3
+    }
+    contents += Swing.VStrut(5)
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += new Label("Gender")
+      contents += Swing.HStrut(20)
+      contents += gender
+    }
+    contents += Swing.VStrut(5)
+    contents += new Label("Comments")
+    contents += Swing.VStrut(3)
+    contents += new ScrollPane(commentField)
+    contents += Swing.VStrut(5)
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += pressMe
+      contents += Swing.HGlue
+      contents += Button("Close") { reportAndClose() }
+    }
+    for (e <- contents) {
+      e.xLayoutAlignment = 0.0
+    }
+    border = Swing.EmptyBorder(10,10,10,10)
   }
 
-  def closeMe(): Unit = {
-    val closeMessage = Dialog.showConfirmation(contents.head, "Are you sure?", optionType = Dialog.Options.YesNo, title=title)
-      if (closeMessage == Dialog.Result.Ok) {
-        sys.exit(0)
-      }
+  def reportAndClose(): Unit = {
+    println("Your name: " + nameField.text)
+    println("Like Scala?: " + likeScala.selected)
+    println("Status1: " + status1.selected)
+    println("Status2: " + status2.selected)
+    println("Status3: " + status3.selected)
+    println("Gender: " + gender.selection.item + " - Index: " + gender.selection.index)
+    println("Comments: " + commentField.text)
+    println("Press Me is pressed: " + pressMe.selected)
+    sys.exit(0)
   }
+
 
 }
