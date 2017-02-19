d59397a9992dee796649081db112ac681aa80280
Bruno Paulino <brunojppb@gmail.com>
2017-02-19 15:48:32 +0100

    modal dialog and user input

--- src/main/scala/ui/UI.scala
+++ src/main/scala/ui/UI.scala
@@ -7,14 +7,35 @@ import scala.swing._
 class UI extends MainFrame {
 
   title = "GUI Program 1"
+
+  val label = new Label("My first UI")
+
   contents = new BoxPanel(Orientation.Vertical) {
-    contents += new Label("Hey I am a label!")
+    contents += label
     contents += Swing.VStrut(10)
     contents += Swing.Glue
-    contents += Button("print some!") { println("THanks!") }
+    contents += Button("Press me!") { pressMeAction() }
     contents += Swing.VStrut(5)
-    contents += Button("Close") { sys.exit(0) }
+    contents += Button("Change Text") { changeTextAction() }
+    contents += Swing.VStrut(5)
+    contents += Button("Close") { closeMe() }
     border = Swing.EmptyBorder(10, 10, 10, 10)
+  }
+
+  def pressMeAction(): Unit = {
+    Dialog.showMessage(contents.head, "Thanks!", title = "You pressed me!")
+  }
+
+  def changeTextAction(): Unit = {
+    val r = Dialog.showInput(contents.head, "Enter New Text:", initial = label.text)
+    r match {
+      case Some(text) => label.text = text
+      case None =>
+    }
+  }
+
+  def closeMe(): Unit = {
+    sys.exit(0)
   }
 
 }
